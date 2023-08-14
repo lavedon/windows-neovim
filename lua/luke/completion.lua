021839d06b2cc2067de0957754063700d2452cec
Luke <lavedon@gmail.com>
2023-08-14 16:40:50 -0400

    luasnips now running

--- lua/luke/completion.lua
+++ lua/luke/completion.lua
@@ -1,4 +1,10 @@
+local cmp = require('cmp')
 local ls = require("luasnip")
+ls.config.set_config({
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+})
+require'luasnip/loaders/from_vscode'.load() -- If you want to use friendly-snippets
 
 ls.snippets = {
   typescript = {
@@ -8,3 +14,51 @@ ls.snippets = {
     ),
   },
 }
+
+local function has_words_before()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+cmp.setup({
+  sources = {
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'nvim_lsp' }, -- if you have lsp setup
+  -- add other sources here
+},
+    -- you can add other sources here, e.g.
+    -- { name = 'buffer' },
+  mapping = {
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif ls.jumpable(-1) then
+        ls.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+  snippet = {
+    expand = function(args)
+      ls.lsp_expand(args.body)
+    end,
+  },
+})
--- lua/luke/packer.lua
+++ lua/luke/packer.lua
@@ -33,6 +33,10 @@ return require('packer').startup(function(use)
     {'hrsh7th/nvim-cmp'},     -- Required
     {'hrsh7th/cmp-nvim-lsp'}, -- Required
     {'L3MON4D3/LuaSnip'},     -- Required
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'rafamadriz/friendly-snippets'},
   }
 }
 use('nvim-tree/nvim-web-devicons')
