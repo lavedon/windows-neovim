3d31f1499ced1c022f11f1fd94ecf4904aeac172
Luke <lavedon@gmail.com>
2024-02-10 12:17:36 -0500

    ag set to work with nvim - externally

--- lua/luke/set.lua
+++ lua/luke/set.lua
@@ -38,3 +38,8 @@ vim.api.nvim_set_keymap('n', '<C-H>', ':Copilot panel<CR>', {noremap = true, sil
 
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+if vim.fn.executable('ag') == 1 then
+  vim.o.grepprg = "ag --vimgrep $*"
+  vim.o.grepformat = "%f:%l:%c:%m"
+end
