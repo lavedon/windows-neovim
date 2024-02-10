99dfa6aa9244436e9b06934297c6a6a3325ea73d
Luke <lavedon@gmail.com>
2024-02-10 11:46:31 -0500

    trying to remove harpoon

--- lua/luke/packer.lua
+++ lua/luke/packer.lua
@@ -19,7 +19,6 @@ return require('packer').startup(function(use)
   }
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
   use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
