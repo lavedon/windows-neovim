40a9f3fd1e1d6b69b8fb377bc3f19b8cc0fcf3d2
Luke <lavedon@gmail.com>
2024-02-14 09:57:32 -0500

    csharp snippets fixed

--- lua/luke/snippets/csharp.lua
+++ lua/luke/snippets/csharp.lua
@@ -15,11 +15,11 @@ ls.add_snippets("cs", {
         parameters.Add("@{Column3}, {variable3});
     ]], {
         Column1 = i(1, "column1"),
-        Variable1 = i(1, "variable1"),
+        variable1 = i(1, "variable1"),
         Column2 = i(1, "column2"),
-        Variable2 = i(1, "variable1"),
+        variable2 = i(1, "variable2"),
         Column3 = i(1, "column3"),
-        Variable2 = i(1, "variable1")
+        variable3 = i(1, "variable3")
     }), {description="Dapper - Create Dynamic parameters"})
 });
        
