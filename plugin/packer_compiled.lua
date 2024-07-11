7ccd83274ebf7267e1f5acd134fbd4ef1018f297
Luke.Avedon <luke.avedon@coniferhealth.com>
2024-07-11 08:01:23 -0400

    still with harpoon

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -115,6 +115,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\Luke.Avedon\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  harpoon = {
+    loaded = true,
+    path = "C:\\Users\\Luke.Avedon\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\harpoon",
+    url = "https://github.com/ThePrimeagen/harpoon"
+  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "C:\\Users\\Luke.Avedon\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lsp-zero.nvim",
@@ -151,7 +156,7 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["obsidian.nvim"] = {
-    config = { "\27LJ\2\nž\1\0\0\5\0\6\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0004\3\3\0005\4\3\0>\4\1\3=\3\5\2B\0\2\1K\0\1\0\15workspaces\1\0\1\15workspaces\0\1\0\2\tname\twork\tpath,C:\\Users\\Luke.Avedon\\ConiferCodingNotes\nsetup\robsidian\frequire\0" },
+    config = { "\27LJ\2\nž\1\0\0\5\0\6\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0004\3\3\0005\4\3\0>\4\1\3=\3\5\2B\0\2\1K\0\1\0\15workspaces\1\0\1\15workspaces\0\1\0\2\tpath,C:\\Users\\Luke.Avedon\\ConiferCodingNotes\tname\twork\nsetup\robsidian\frequire\0" },
     loaded = true,
     path = "C:\\Users\\Luke.Avedon\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\obsidian.nvim",
     url = "https://github.com/epwalsh/obsidian.nvim"
@@ -200,7 +205,7 @@ try_loadstring("\27LJ\2\nG\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B
 time([[Config for CopilotChat.nvim]], false)
 -- Config for: obsidian.nvim
 time([[Config for obsidian.nvim]], true)
-try_loadstring("\27LJ\2\nž\1\0\0\5\0\6\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0004\3\3\0005\4\3\0>\4\1\3=\3\5\2B\0\2\1K\0\1\0\15workspaces\1\0\1\15workspaces\0\1\0\2\tname\twork\tpath,C:\\Users\\Luke.Avedon\\ConiferCodingNotes\nsetup\robsidian\frequire\0", "config", "obsidian.nvim")
+try_loadstring("\27LJ\2\nž\1\0\0\5\0\6\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0004\3\3\0005\4\3\0>\4\1\3=\3\5\2B\0\2\1K\0\1\0\15workspaces\1\0\1\15workspaces\0\1\0\2\tpath,C:\\Users\\Luke.Avedon\\ConiferCodingNotes\tname\twork\nsetup\robsidian\frequire\0", "config", "obsidian.nvim")
 time([[Config for obsidian.nvim]], false)
 
 _G._packer.inside_compile = false
