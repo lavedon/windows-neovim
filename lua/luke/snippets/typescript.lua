a33752b2567385089661057f693b8e1b5d9d70c2
Luke.Avedon <luke.avedon@coniferhealth.com>
2024-10-14 13:38:00 -0400

    new console assert snippet

--- lua/luke/snippets/typescript.lua
+++ lua/luke/snippets/typescript.lua
@@ -7,6 +7,11 @@ ls.add_snippets("typescript", {
 	s("ternary", {
 		-- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
-	})
+	}),
+    s("cassert", {
+      t("console.assert("), i(1, "variable"), t(".length !== 0, '"), rep(1), t(" is empty.');\n"),
+      t("console.assert("), rep(1), t(" !== undefined, '"), rep(1), t(" is undefined.');\n"),
+      t("console.assert("), rep(1), t(" !== null, '"), rep(1), t(" is null.');"),
+    })
 })
 
