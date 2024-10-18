af827c5d95e697213e2dbdfd245b3f9b2bfdecca
Luke.Avedon <luke.avedon@coniferhealth.com>
2024-10-18 08:54:42 -0400

    updates

--- lua/luke/snippets/typescript.lua
+++ lua/luke/snippets/typescript.lua
@@ -4,16 +4,66 @@ local t = ls.t
 local i = ls.i 
 
 print("Loading typescript snippets.")
-ls.add_snippets("ts", {
+ls.add_snippets("typescript", {
 	s("ternary", {
 		-- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
 	}),
-    s("cassert", {
-      t("console.assert("), i(1, "variable"), t(".length !== 0, '"), rep(1), t(" is empty.');\n"),
-      t("console.assert("), rep(1), t(" !== undefined, '"), rep(1), t(" is undefined.');\n"),
-      t("console.assert("), rep(1), t(" !== null, '"), rep(1), t(" is null.');"),
-    })
+    s("cdir", fmt([[
+    console.assert({}.length !== 0, '{} is empty.');
+    console.assert({} !== undefined, '{} is undefined.');
+    console.assert({} !== null, '{} is null.');
+    console.log('{} is ...');
+    console.dir({});
+        ]], {
+            i(1, "variable"),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1)
+    })),
+    s("cassert", fmt([[
+    console.assert({}.length !== 0, '{} is empty.');
+    console.assert({} !== undefined, '{} is undefined.');
+    console.assert({} !== null, '{} is null.');
+        ]], {
+            i(1, "variable"),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1),
+            rep(1)
+    })),
+    s("dir", fmt([[
+    console.log('{} is ...');
+    console.dir({});
+    ]], {
+        i(1, "variable"),
+        rep(1)
+    })),
+    s("caevery", fmt([[
+        console.assert(
+           {}.every(x => x.{} && x.{} !== ''),
+        'Not every item has a non-empty {}.'
+        );
+    ]], {
+        i(1, "object"),
+        i(2, "property"),
+        rep(2),
+        rep(2)
+    })),
+    s("isn", fmt([[
+        console.assert(typeof {} === 'number', '{} is not a number');
+        console.assert({}.length > 0, '{} is not greater than 0');
+    ]], {
+        i(1, 'property'),
+        rep(1),
+        rep(1),
+        rep(1)
+    }))
 })
 print("Typescript snippets loaded.");
 
