8089ea368749c7c633cd3d7375fb0d9d39571a38
Luke.Avedon <luke.avedon@coniferhealth.com>
2024-10-21 13:04:36 -0400

    typescript - new snippet a property matches between 2 objects

--- lua/luke/snippets/typescript.lua
+++ lua/luke/snippets/typescript.lua
@@ -5,6 +5,19 @@ local i = ls.i
 
 print("Loading typescript snippets.")
 ls.add_snippets("typescript", {
+    s("atleastOne", fmt([[
+    const atLeastOneMatch = {}.some(obj1 =>
+        {}.some(obj2 => obj1.{} === obj2.{}));
+    console.warn('any matches between {} and {}');
+    console.warn(atLeastOneMatch);
+    ]], {
+        i(1, "collection1"),
+        i(2, "collection2"),
+        i(3, "col1.obj.property"),
+        i(4, "col2.obj.matchProperty"),
+        rep(1),
+        rep(2)
+    }),
 	s("ternary", {
 		-- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
